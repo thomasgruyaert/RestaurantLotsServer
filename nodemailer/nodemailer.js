52070074b727f9620f0f4fa03247c877849e282a
thomas.gruyaert <thomas.gruyaert@liantis.be>
2026-01-24 20:58:32 +0100

    Fix for mailing pt4

--- nodemailer/nodemailer.js
+++ nodemailer/nodemailer.js
@@ -50,46 +50,41 @@ async function sendResApprovalMailClient(requestorEmail, reservation) {
   };
   var reservationDateString = reservationDateTime.toLocaleTimeString('nl-BE',
       options);
-  mailOptions = {
-    from: { name: "Restaurant Lots", email: process.env.SENDER_EMAIL },
-    to: requestorEmail,
-    subject: `Restaurant Lots - Bevestiging Reservatie #${reservation.id}`,
-    template: 'reservationConfirmationClient',
-    context: {
-      srcLotsBanner: `${process.env.FIREBASE_URL}${encodeURIComponent(
-          'mail/reservation/gallery (22).jpeg')}?alt=media`,
-      srcLotsLogo: `${process.env.FIREBASE_URL}${encodeURIComponent(
-          'mail/reservation/Transparent-1.png')}?alt=media`,
-      srcLotsLocatie: `${process.env.FIREBASE_URL}${encodeURIComponent(
-          'mail/reservation/Group-4.png')}?alt=media`,
-      srcResApproved: `${process.env.FIREBASE_URL}${encodeURIComponent(
-          'mail/reservation/approved.png')}?alt=media`,
-      reservationFullName: reservation.firstName + " " + reservation.lastName,
-      reservationId: reservation.id,
-      reservationNPeople: reservation.nPeople,
-      reservationMobileNr: reservation.mobileNr,
-      reservationEmail: reservation.email,
-      reservationDateString: reservationDateString,
-      reservationMealType: reservation.mealType === 'dinner' ? 'Diner'
-          : 'Lunch',
-      reservationPayWithVoucher: reservation.payWithVoucher ? 'ja' : 'neen',
-      reservationAdditionalComments: reservation.additionalComments
-          ? reservation.additionalComments : '/',
-    }
+  let context = {
+    srcLotsBanner: `${process.env.FIREBASE_URL}${encodeURIComponent(
+        'mail/reservation/gallery (22).jpeg')}?alt=media`,
+    srcLotsLogo: `${process.env.FIREBASE_URL}${encodeURIComponent(
+        'mail/reservation/Transparent-1.png')}?alt=media`,
+    srcLotsLocatie: `${process.env.FIREBASE_URL}${encodeURIComponent(
+        'mail/reservation/Group-4.png')}?alt=media`,
+    srcResApproved: `${process.env.FIREBASE_URL}${encodeURIComponent(
+        'mail/reservation/approved.png')}?alt=media`,
+    reservationFullName: reservation.firstName + " " + reservation.lastName,
+    reservationId: reservation.id,
+    reservationNPeople: reservation.nPeople,
+    reservationMobileNr: reservation.mobileNr,
+    reservationEmail: reservation.email,
+    reservationDateString: reservationDateString,
+    reservationMealType: reservation.mealType === 'dinner' ? 'Diner'
+        : 'Lunch',
+    reservationPayWithVoucher: reservation.payWithVoucher ? 'ja' : 'neen',
+    reservationAdditionalComments: reservation.additionalComments
+        ? reservation.additionalComments : '/',
   }
-  const html = await renderTemplate("reservationConfirmationClient", context);
 
-  return await client.send({
-    from: {name: "Restaurant Lots", email: process.env.SENDER_EMAIL},
-    to: [{email: requestorEmail}],
-    subject: `Restaurant Lots - Bevestiging Reservatie #${reservation.id}`,
-    html,
-    text: `Je reservatie #${reservation.id} is bevestigd op ${reservationDateString}.`,
-  });
+const html = await renderTemplate("reservationConfirmationClient", context);
+
+return await client.send({
+  from: {name: "Restaurant Lots", email: process.env.SENDER_EMAIL},
+  to: [{email: requestorEmail}],
+  subject: `Restaurant Lots - Bevestiging Reservatie #${reservation.id}`,
+  html,
+  text: `Je reservatie #${reservation.id} is bevestigd op ${reservationDateString}.`,
+});
 }
 
 async function sendResRefusalMailClient(requestorEmail, refusalReason) {
-  context = {
+  let context = {
     srcLotsBanner: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/gallery (22).jpeg')}?alt=media`,
     srcLotsLogo: `${process.env.FIREBASE_URL}${encodeURIComponent(
@@ -102,8 +97,8 @@ async function sendResRefusalMailClient(requestorEmail, refusalReason) {
   }
   const html = await renderTemplate("reservationRefusalClient", context);
   return await client.send({
-    from: { name: "Restaurant Lots", email: process.env.SENDER_EMAIL },
-    to: [{ email: requestorEmail }],
+    from: {name: "Restaurant Lots", email: process.env.SENDER_EMAIL},
+    to: [{email: requestorEmail}],
     subject: `Restaurant Lots - Reservatie geweigerd`,
     html,
     text: `Jouw reservatie werd geweigerd. Reden: ${refusalReason}`
@@ -111,21 +106,20 @@ async function sendResRefusalMailClient(requestorEmail, refusalReason) {
 }
 
 async function sendResPendingMailClient(requestorEmail) {
-  console.log("send res pending mail client to " + requestorEmail);
-  context = {
+  let context = {
     srcLotsBanner: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/gallery (22).jpeg')}?alt=media`,
-        srcLotsLogo: `${process.env.FIREBASE_URL}${encodeURIComponent(
+    srcLotsLogo: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/Transparent-1.png')}?alt=media`,
-        srcLotsLocatie: `${process.env.FIREBASE_URL}${encodeURIComponent(
+    srcLotsLocatie: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/Group-4.png')}?alt=media`,
-        srcResPending: `${process.env.FIREBASE_URL}${encodeURIComponent(
+    srcResPending: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/pending.png')}?alt=media`,
   }
   const html = await renderTemplate("reservationPendingClient", context);
   return await client.send({
-    from: { name: "Restaurant Lots", email: process.env.SENDER_EMAIL },
-    to: [{ email: requestorEmail }],
+    from: {name: "Restaurant Lots", email: process.env.SENDER_EMAIL},
+    to: [{email: requestorEmail}],
     subject: `Restaurant Lots - Reservatie verstuurd`,
     html,
     text: "Jouw reservatie werd verstuurd, maar moet nog goedgekeurd worden."
@@ -145,32 +139,32 @@ async function sendResPendingMailLots(reservation) {
   };
   var reservationDateString = reservationDateTime.toLocaleTimeString('nl-BE',
       options);
-  context = {
+  let context = {
     srcResPending: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/pending.png')}?alt=media`,
-        srcLotsBanner: `${process.env.FIREBASE_URL}${encodeURIComponent(
+    srcLotsBanner: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/gallery (22).jpeg')}?alt=media`,
-        srcLotsLogo: `${process.env.FIREBASE_URL}${encodeURIComponent(
+    srcLotsLogo: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/Transparent-1.png')}?alt=media`,
-        srcLotsLocatie: `${process.env.FIREBASE_URL}${encodeURIComponent(
+    srcLotsLocatie: `${process.env.FIREBASE_URL}${encodeURIComponent(
         'mail/reservation/Group-4.png')}?alt=media`,
-        reservationFullName: reservation.firstName + " " + reservation.lastName,
-        reservationConfirmationCode: reservation.confirmationCode,
-        reservationId: reservation.id,
-        reservationNPeople: reservation.nPeople,
-        reservationMobileNr: reservation.mobileNr,
-        reservationEmail: reservation.email,
-        reservationDateString: reservationDateString,
-        reservationMealType: reservation.mealType === 'dinner' ? 'Diner'
+    reservationFullName: reservation.firstName + " " + reservation.lastName,
+    reservationConfirmationCode: reservation.confirmationCode,
+    reservationId: reservation.id,
+    reservationNPeople: reservation.nPeople,
+    reservationMobileNr: reservation.mobileNr,
+    reservationEmail: reservation.email,
+    reservationDateString: reservationDateString,
+    reservationMealType: reservation.mealType === 'dinner' ? 'Diner'
         : 'Lunch',
-        reservationPayWithVoucher: reservation.payWithVoucher ? 'ja' : 'neen',
-        reservationAdditionalComments: reservation.additionalComments
+    reservationPayWithVoucher: reservation.payWithVoucher ? 'ja' : 'neen',
+    reservationAdditionalComments: reservation.additionalComments
         ? reservation.additionalComments : '/',
   }
   const html = await renderTemplate("reservationPendingLots", context);
   return await client.send({
-    from: { name: "Restaurant Lots", email: process.env.NO_REPLY_EMAIL },
-    to: [{ email: process.env.SENDER_EMAIL }],
+    from: {name: "Restaurant Lots", email: process.env.NO_REPLY_EMAIL},
+    to: [{email: process.env.SENDER_EMAIL}],
     subject: `Online Reservatie - Aanvraag Reservatie #${reservation.id}`,
     html,
     text: `Reservatie met ID #${reservation.id} werd aangevraagd`
@@ -186,36 +180,21 @@ async function sendVoucherMail(voucher, pdfPath) {
     day: 'numeric'
   };
   var boughtDateString = boughtDate.toLocaleDateString('nl-BE', options);
-  context = {
+  let context = {
     voucherNameReceivers: voucher.nameReceivers,
-        voucherNameGifters: voucher.nameGifters,
-        voucherBoughtDate: boughtDateString,
-        voucherAmount: voucher.voucherAmount,
-        voucherCustomMessage: voucher.customMessage
+    voucherNameGifters: voucher.nameGifters,
+    voucherBoughtDate: boughtDateString,
+    voucherAmount: voucher.voucherAmount,
+    voucherCustomMessage: voucher.customMessage
   };
   const html = await renderTemplate("voucherMail", context);
 
   const pdfBuffer = await fs.readFile(pdfPath);
   const pdfBase64 = pdfBuffer.toString("base64");
 
-  mailOptions = {
-    from: { name: "Restaurant Lots", email: process.env.SENDER_EMAIL },
-    to: `${voucher.emailRecipient}`,
-    subject: `Restaurant Lots - Jouw geschenkbon`,
-    template: 'voucherMail',
-
-    attachments: [
-      {
-        filename: `${voucher.id}_voucher.pdf`,
-        path: pdfPath,
-        contentType: 'application/pdf'
-      }
-    ]
-  }
-
   return await client.send({
-    from: { name: "Restaurant Lots", email: process.env.SENDER_EMAIL },
-    to: [{ email: voucher.emailRecipient }],
+    from: {name: "Restaurant Lots", email: process.env.SENDER_EMAIL},
+    to: [{email: voucher.emailRecipient}],
     subject: "Restaurant Lots - Jouw geschenkbon",
     html,
     text: `Dag ${voucher.nameReceivers}, hierbij je geschenkbon van Restaurant Lots.`,
